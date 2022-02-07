8494d8f09e2b44b3182ef3edbd0cfaab18a0fcdc
kentoktm <kento.nakazawa@rakus-partners.co.jp>
2022-02-07 14:20:36 +0900

    InsertAdministratorForm 作成

---
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -0,0 +1,48 @@
+package jp.co.sample.form;
+
+public class InsertAdministratorForm {
+
+    /**
+     * 名前
+     */
+    private String name;
+    /**
+     * メルアド
+     */
+    private String mailAddress;
+    /**
+     * パスワード
+     */
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "InsertAdministratorForm [mailAddress=" + mailAddress + ", name=" + name + ", password=" + password
+                + "]";
+    }
+
+}
